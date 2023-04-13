e69b1003702bbed5333c75236c460693309d4c17
HariniMurugan-2003 <112229253+HariniMurugan-2003@users.noreply.github.com>
2023-04-13 13:21:06 +0530

    overall

--- Ordinal/index.js
+++ Ordinal/index.js
@@ -228,10 +228,11 @@ const questions = [
 ]
 
 
-let shuffledQuestions = [] //empty array to hold shuffled selected questions
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
 
 function handleQuestions() { 
     //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
     while (shuffledQuestions.length <= 9) {
         const random = questions[Math.floor(Math.random() * questions.length)]
         if (!shuffledQuestions.includes(random)) {
@@ -241,12 +242,13 @@ function handleQuestions() {
 }
 
 
-let questionNumber = 1
-let playerScore = 0  
-let wrongAttempt = 0 
-let indexNumber = 0
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
 
 // function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
 function NextQuestion(index) {
     handleQuestions()
     const currentQuestion = shuffledQuestions[index]
@@ -273,7 +275,7 @@ function checkForAnswer() {
             correctOption = option.labels[0].id
         }
     })
-   
+
     //checking to make sure a radio input has been checked or an option being chosen
     if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
         document.getElementById('option-modal').style.display = "flex"
@@ -283,8 +285,8 @@ function checkForAnswer() {
     options.forEach((option) => {
         if (option.checked === true && option.value === currentQuestionAnswer) {
             document.getElementById(correctOption).style.backgroundColor = "green"
-            playerScore++
-            indexNumber++
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
             //set to delay question number till when next question loads
             setTimeout(() => {
                 questionNumber++
@@ -295,7 +297,7 @@ function checkForAnswer() {
             const wrongLabelId = option.labels[0].id
             document.getElementById(wrongLabelId).style.backgroundColor = "red"
             document.getElementById(correctOption).style.backgroundColor = "green"
-            wrongAttempt++
+            wrongAttempt++ //adds 1 to wrong attempts 
             indexNumber++
             //set to delay question number till when next question loads
             setTimeout(() => {
@@ -309,15 +311,16 @@ function checkForAnswer() {
 
 //called when the next button is called
 function handleNextQuestion() {
-    checkForAnswer()
+    checkForAnswer() //check if player picked right or wrong option
     unCheckRadioButtons()
-    //delays next question displaying for a second
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
     setTimeout(() => {
         if (indexNumber <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
             NextQuestion(indexNumber)
         }
         else {
-            handleEndGame()
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
         }
         resetOptionBackground()
     }, 1000);
@@ -346,30 +349,28 @@ function handleEndGame() {
 
     // condition check for player remark and remark color
     if (playerScore <= 3) {
-        remark = "Bad Grades, Keep Practicing."
-        remarkColor = "red"
-    }
-    else if (playerScore >= 4 && playerScore < 7) {
-        remark = "Average Grades, You can do better."
-        remarkColor = "orange"
-    }
-    else if (playerScore >= 7) {
-        remark = "Excellent, Keep the good work going."
+        remark = "Thank You for taking our quiz. You seem to be doing well considering everything you have been through. Keep going and be the best version of yourself. Healing is not done in a day but you seem to find your way through it pretty well. We are very proud of you for everything you have achieved. "
         remarkColor = "green"
+    }
+   
+    
+    else if (playerScore >= 7) {
+        remark = "Thank You for taking our quiz. We are very sorry to inform you but we feel like you will do better if you get some help to deal with your traumas and low points. But that being said, we really appreciate your efforts to get better by taking small steps like this test. "
+        remarkColor = "red"
     }
     const playerGrade = (playerScore / 10) * 100
 
-    //data to display to score board
+    
     document.getElementById('remarks').innerHTML = remark
     document.getElementById('remarks').style.color = remarkColor
-    document.getElementById('grade-percentage').innerHTML = playerGrade
-    document.getElementById('wrong-answers').innerHTML = wrongAttempt
-    document.getElementById('right-answers').innerHTML = playerScore
-    document.getElementById('score-modal').style.display = "flex"
+    //document.getElementById('grade-percentage').innerHTML = playerGrade
+    //document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    //document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex" 
 
 }
 
-//closes score modal and resets game
+//closes score modal, resets game and reshuffles questions
 function closeScoreModal() {
     questionNumber = 1
     playerScore = 0
@@ -384,6 +385,3 @@ function closeScoreModal() {
 function closeOptionModal() {
     document.getElementById('option-modal').style.display = "none"
 }
- 
- 
- 
